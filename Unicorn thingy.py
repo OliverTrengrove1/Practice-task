bdef471a63695567cb4077458ea95d32663f86bd
OliverTrengrove1 <trengroveo2@middleton.school.nz>
2022-03-30 22:35:31 +1300

    initial pull

--- Unicorn thingy.py
+++ Unicorn thingy.py
@@ -1,68 +1,55 @@
-#Lucky Unicorn Fully Working Program
-#Program should work - neds to be tested for usability
+'''
+Oliver Trengrove Lucky unicorn game
+this game allows users to bet money and win based on what token is returned
+
+'''
 
 import random
+# the initial bet for each round is one dollar, the value of this variable can be changed based on needs
+bet = 1
+total_bet = 0
+session_limit = 10
+total_won = 0
+current_win = 0
+token_list=["donkey","horse","zebra","unicorn"]
+print("Welcome to Oliver's lucky unicorn game \n"
+      "Each round costs $" + str(bet) + ", you can only spend up to $10")
+keep_going = "y"
 
-#Integer checking function below
-def intcheck(question, low, high):
-  valid = False
-  while not valid:
-    error = "Whoops! Please enter an integer between {} and {}.".format(low, high)
-
-    try:
-      response = int(input("Enter an integer between {} and {}: ".format(low, high)))
-
-      if low <= response <= high:
-        return response
-      else:
-        print(error)
-        print()
-
-    except ValueError:
-      print(error)
-
-#main routine goes here
-
-
-
- #Ask user how much they want to play with (min $1, max $10)
-balance = intcheck("How much money do you want to play with? ",1, 10)
-
-keep_going = ""
-while keep_going == "":
-
-  #Tokens list includes 10 items to prevent too many unicorns being chosen
-  tokens = ["horse", "horse","horse",
-  "zebra","zebra","zebra",
-  "donkey","donkey","donkey", "unicorn"]
-
-  #Randomly choose a token from our list above
-  token = random.choice(tokens)
-  print()
-  print("You got a {}".format(token))
-
-  #Adjust balance based on the chosen token and generate feedback
-  if token == "unicorn":
-    balance += 4
-    feedback = "Congratulations you won $5.00."
-  elif token == "donkey":
-    balance -= 1
-    feedback = "Sorry, you did not win anything this round."
+# all the code for the game sits in the while loop. at the start of each round the user is asked if they want to play
+while keep_going == "y":
+  # check if the user is trying to bet more than the session limit, if so quit
+  if total_bet >= session_limit:
+    print("sorry you have reached the betting limit")
+    break
+  keep_going = input("Would you like to play a round? Press y, to quit press any other key")
+  # checking that the user wants to play the game
+  if keep_going != "y":
+    break
+  total_bet += 1
+  chosen_token = random.choice(token_list)
+  print("You got a {}".format(chosen_token))
+  # calculate the money won based of the selected token
+  if chosen_token =='zebra'or chosen_token =='horse':
+    current_win = 0.5
+    total_won += current_win
+  elif chosen_token == 'unicorn':
+    current_win = 5
+    total_won += current_win
   else:
-    balance -= 0.5
-    feedback = "Congratulation you won 50c."
+    current_win = 0
 
-  print()
-
-  print(feedback)
-  print("You have ${:.2f} to play with".format(balance))
-
-
-  if balance <1:
+  print('you have won a total of $'+str(total_won))
+  print('you have bet a total of $'+str(total_bet))
+  if total_won <0:
     print("Sorry, you don't have enough money to continue. Gave over.")
     keep_going = "end"
-  else:
-    keep_going = input("Press <enter> to play again or any key to quit.")
 
-#Farewell user at end of game
-print("Thank you for playing.")
+
+
+
+
+
+
+
+
